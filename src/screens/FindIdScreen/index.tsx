a4fc23c394384b8f41111491f4cb679e0952c224
slogup <slogup@slogupui-MacBookAir-3.local>
2022-06-28 09:54:20 +0900

    Bono push test

---
+++ src/screens/FindIdScreen/index.tsx
@@ -0,0 +1,11 @@
+import React, { FunctionComponent } from 'react';
+import { SafeAreaView } from 'react-native';
+
+// eslint-disable-next-line @typescript-eslint/no-empty-interface
+interface Props {}
+
+const FindidScreen: FunctionComponent<Props> = function FindidScreen() {
+  return <SafeAreaView />;
+};
+
+export default FindidScreen;
